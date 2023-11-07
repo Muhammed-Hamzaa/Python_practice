97636c21458483628dffbcf9e0e3cf2aad69e6d7
Muhammad Hamza <hamzaghia14@gmail.com>
2023-11-07 22:39:54 +0500

    os module

---
+++ practice/os_module .py
@@ -0,0 +1,28 @@
+# import os
+
+
+
+#for creating folder
+# if(not os.path.exists("data")):
+#     os.mkdir("data")
+# for i in range(0, 100):
+#     os.mkdir(f"data/Day{i+1}")
+     
+
+
+
+#folder in a folder
+
+# folders = os.listdir("data")
+# for folder in folders:
+#     print(folder)
+#     print(os.listdir(f"data/{folder}"))
+
+
+
+ 
+#for re-Naming
+
+# for i in range(0, 100):
+#     os.rename(f"data/Tutorial{i+1}", f"data/Tutorial {i+1}")
+    
